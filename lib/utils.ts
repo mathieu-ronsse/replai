ab7342818d472e59f217eef7e8e39b244bc35d26
mathieu-ronsse <mathieu.ronsse@ipsos.com>
2024-10-16 21:07:10 +0200

    added ts-ignore

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,7 @@
 /* eslint-disable prefer-const */
 /* eslint-disable no-prototype-builtins */
 import { type ClassValue, clsx } from "clsx";
+// @ts-ignore
 import qs from "qs";
 import { twMerge } from "tailwind-merge";
 
