8716bb4013b4a5c721db2f971246c2c388d54873
mathieu-ronsse <mathieu.ronsse@ipsos.com>
2024-10-18 17:38:21 +0200

    back to username

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -63,7 +63,7 @@ export async function POST(req: Request) {
     const user = {
       clerkId: id,
       email: email_addresses[0].email_address,
-      userName: username!,
+      username: username!,
       firstName: first_name,
       lastName: last_name,
       photo: image_url,
@@ -90,7 +90,7 @@ export async function POST(req: Request) {
     const user = {
       firstName: first_name,
       lastName: last_name,
-      userName: username!,
+      username: username!,
       photo: image_url,
     };
 
