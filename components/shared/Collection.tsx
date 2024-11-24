640ffac55992f728aa411e672efb0b63c494ca99
mathieu-ronsse <mathieu.ronsse@ipsos.com>
2024-11-24 21:26:52 +0100

    tryfix key error collections

--- components/shared/Collection.tsx
+++ components/shared/Collection.tsx
@@ -54,7 +54,7 @@ export const Collection = ({
       {images.length > 0 ? (
         <ul className="collection-list">
           {images.map((image) => (
-            <Card image={image} key={image._id} />
+            <Card image={image} key={image._id as string} /> // "as string" added
           ))}
         </ul>
       ) : (
